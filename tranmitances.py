d70d77115e9b112150c470962103755f5fa77ca6
PiGiKeY <piotr.koruba@pwr.edu.pl>
2025-02-19 12:13:13 +0100

    New code for transmitance

---
+++ tranmitances.py
@@ -0,0 +1,21 @@
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+
+# Definicja transmitancji: H(s) = (s + 1) / (s^2 + 2s + 1)
+numerator = [1, 1]  # Licznik
+denominator = [1, 2, 1]  # Mianownik
+
+# Tworzenie obiektu transmitancji
+system = signal.TransferFunction(numerator, denominator)
+
+# Analiza odpowiedzi skokowej
+t, response = signal.step(system)
+
+# Wykres odpowiedzi skokowej
+plt.plot(t, response)
+plt.title("Odpowiedź skokowa systemu")
+plt.xlabel("Czas [s]")
+plt.ylabel("Odpowiedź")
+plt.grid()
+plt.show()
